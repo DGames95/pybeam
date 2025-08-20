31c1b1eb1ea5caafc4c1b9092f6650a7df8c5ec1
Damian <d.l.games@student.tudelft.nl>
2025-08-20 19:19:07 +0200

    feat: add loads, analyze and visualizer to init

--- pybeam/__init__.py
+++ pybeam/__init__.py
@@ -3,9 +3,15 @@ print("dev version pybeam 0.0.1")
 from . import members
 from . import profiles
 from . import materials
+from . import loads
+from . import analyze
+from . import visualizers
 
 __all__ = [
     "members",
     "profiles",
-    "materials"
+    "materials",
+    "loads",
+    "analyze",
+    "visualizers"
 ]
