e3b09392cb810e4c329362bbfce16bb4e20d3659
Damian <d.l.games@student.tudelft.nl>
2025-08-21 17:20:17 +0200

    test: add manaul integration test of saving feature

--- test/integration_test/test_plot.py
+++ test/integration_test/test_plot.py
@@ -1,7 +1,8 @@
 import pytest
+import pybeam
 from pybeam import analyze, visualizers, loading_case, loads
 
-@pytest.mark.skip(reason="Manual Test")
+@pytest.mark.skip(reason="Manual Test, check that plot is shown correctly")
 def test_plotting_point_load():
     point_force = loads.PointForce(20, 0)
     point_force_2 = loads.PointForce(20, 1)
@@ -12,6 +13,20 @@ def test_plotting_point_load():
     analyzer = analyze.BeamAnalyzer(supported_beam)
 
     analyzer.visualize(visualizers.MatplotlibVisualizer())
+
+@pytest.mark.skip(reason="Manual Test, check that plot is saved")
+def test_saving_plot_member():
+    profile1 = pybeam.profiles.IBeamProfile(30, 20, 0.01, 0.01)
+
+    material1 = pybeam.materials.Steel()
+
+    member1 = pybeam.members.UniformMember(5, profile1, material1)
+
+    member1.add_shear_load(10, 0.5)
+
+    member1.plot()
+
+    member1.save_plot("test1")
 
 if __name__=="__main__":
     test_plotting_point_load()
